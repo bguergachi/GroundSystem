b9a059c192e6994553f770a8de83a0ba4ae00367
agsof <bguergachi@gmail.com>
2017-01-24 23:29:46 -0500

    some fixes in variable naming extra coments

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from PIL import ImageTk,Image
 # resolution of the screen
 height = 480
 width = 320
-statusBG = 'cyan'
+statusBackGround = 'cyan'
 
 
 
@@ -26,16 +26,18 @@ class Display:
         self.__master.resizable(width=False, height=False)
         self.__master.geometry('{}x{}'.format(height, width))
         #To make window borderless
-        # master.overrideredirect(True)
+        #master.overrideredirect(True)
+
+        #Variables for time values
+        self.__currentTime = StringVar()
+        self.__lastMesgTime = StringVar()
+
 
         self.printLabel()
         self.statusBar()
         self.SettingsImageButtons()
         self.scrollButtons()
 
-
-        #Don't worry about this for now
-        #self.__master.after(200,lambda: self.__init__(master))
 
 
 
@@ -45,11 +47,10 @@ class Display:
     def printLabel(self):
         # Print label of time and time of last received message
         time = Label(self.__master,
-                     text="Time:  " + self.getTime()[0] + "\t\t\t" + "Time of Last Message:  " + self.getTime()[1],
+                     text="Time:  " + self.getTime()[0].get() + "\t\t\t" + "Time of Last Message:  " + self.getTime()[1].get(),
                      bd=1, relief=SUNKEN,width = 60)
         time.pack_propagate(False)
         time.pack(side=TOP, anchor=W)
-
 
 
 
@@ -62,17 +63,17 @@ class Display:
         status.pack(side=BOTTOM, fill="x")
 
         # Frame with map information
-        mapCoordinates = Frame(status, bg=statusBG, height=70, width=height / 3 - 4)
+        mapCoordinates = Frame(status, bg=statusBackGround, height=70, width=height / 3 - 4)
         mapCoordinates.pack(side=LEFT, pady=2, padx=2)
         mapCoordinates.pack_propagate(False)
 
         # Frame containing timer information and separation status
-        statusStopWatch = Frame(status, bg=statusBG, height=70, width=height / 3 - 4)
+        statusStopWatch = Frame(status, bg=statusBackGround, height=70, width=height / 3 - 4)
         statusStopWatch.pack(side=LEFT, pady=2, padx=2)
         statusStopWatch.pack_propagate(False)
 
         # Frame containing altitude and pressure information
-        altitudePressure = Frame(status, bg=statusBG, height=70, width=height / 3 - 4)
+        altitudePressure = Frame(status, bg=statusBackGround, height=70, width=height / 3 - 4)
         altitudePressure.pack(side=LEFT, pady=2, padx=2)
         altitudePressure.pack_propagate(False)
 
@@ -82,31 +83,32 @@ class Display:
         render = ImageTk.PhotoImage(load)
 
         #Paint compass image in label with parent being frame with map information
-        img = Label(mapCoordinates,image = render,bg=statusBG)
+        img = Label(mapCoordinates,image = render,bg=statusBackGround)
         img.image = render
         img.pack(side=LEFT,anchor=W,pady=2,padx=8)
 
         #Set text label with coordinates with parent being frame with map information
         latitude_longitude = Label(mapCoordinates, text = self.getLatitude()+",\n"+self.getLongitude(),bg="cyan",fg="black")
+        #latitude_longitude.bind("<Button-1>", self.__currentTime = "Hello")
         latitude_longitude.pack(side=LEFT,anchor=W,padx=8)
 
         #Paint stopwatch timer
-        stopWatch = Label(statusStopWatch, text = self.getStopWatch(),bg = statusBG,fg="black")
+        stopWatch = Label(statusStopWatch, text = self.getStopWatch(),bg = statusBackGround,fg="black")
         stopWatch.config(font=("times",20))
         stopWatch.pack(side=TOP,anchor=N)
 
         #Paint status indication for separation
-        statusFlag = Label(statusStopWatch,text="status",bg = statusBG,fg = self.getStatusFlag())
+        statusFlag = Label(statusStopWatch,text="status",bg = statusBackGround,fg = self.getStatusFlag())
         statusFlag.config(font=("times",15))
         statusFlag.pack(side=TOP,anchor=S)
 
         #Paint label of altitude or pressure value
-        altitude_pressure = Label(altitudePressure, text=self.getAltitudePressureData() + "m", bg = statusBG)
+        altitude_pressure = Label(altitudePressure, text=self.getAltitudePressureData() + "m", bg = statusBackGround)
         altitude_pressure.config(font=("times,20"))
         altitude_pressure.pack(side = TOP,anchor=N)
 
         #Paint descriptive label of data
-        title_of_data = Label(altitudePressure,text=self.getAltitudePressureTitle(),bg = statusBG)
+        title_of_data = Label(altitudePressure,text=self.getAltitudePressureTitle(),bg = statusBackGround)
         title_of_data.config(font=("times",20))
         title_of_data.pack(side=TOP)
 
@@ -147,11 +149,12 @@ class Display:
         return "-79.732410"
 
     def getTime(self):
-        currentTime = str(random.randint(0, 24)) + ":" + str(random.randint(0, 60)) + ":" + str(
-            random.randint(0, 60))  # Current time to display
-        lastMesgTime = str(random.randint(0, 24)) + ":" + str(random.randint(0, 60)) + ":" + str(
-            random.randint(0, 60))  # Time of last message
-        return [currentTime, lastMesgTime]
+
+        self.__currentTime.set(str(random.randint(0, 24)) + ":" + str(random.randint(0, 60)) + ":" + str(
+            random.randint(0, 60))) # Current time to display
+        self.__lastMesgTime.set(str(random.randint(0, 24)) + ":" + str(random.randint(0, 60)) + ":" + str(
+            random.randint(0, 60)))  # Time of last message
+        return [self.__currentTime, self.__lastMesgTime]
 
     def getStopWatch(self):
         return "01:23.1"
@@ -171,6 +174,10 @@ class Display:
 root = Tk()
 display = Display(root)
 root.mainloop()
+
+
+
+
 
 
 
