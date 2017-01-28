d0d9b6fa777b100bdb84cdad84acf875296c4db2
ZavSer <ma.zavalaserret@gmail.com>
2017-01-28 14:17:06 -0500

    DONE: changed geometry added cancel button ToDo: confirm changes pop up confirm cancel pop up change color when hover

--- Settings.py
+++ Settings.py
@@ -2,9 +2,13 @@
 
 #@author: Alejandra Zavala
 
-
 from tkinter import *
 global imp, met, portLab, ser1,ser2,ser3
+
+#***************FUNCTIONS************************
+
+def hover_color(widget, color, event): # function changes color of button when hovering above it
+    widget.config(foreground=color)
 
 
 
@@ -13,34 +17,43 @@ root.geometry('422x250') # this function defines the size of the window
 root.title('Settings Menu') # this function names the overall window
 root.configure( background = "snow",)
 
-Frame1 = Frame(root, bg="khaki")
-Frame1.grid(row = 0, column = 0, rowspan = 3, columnspan = 1, sticky = W+E+N+S)
+Frame1 = Frame(root, bg="khaki",relief="sunken", width=100, height=220)
+Frame1.grid(row = 0, column = 0, rowspan = 3, columnspan = 2, sticky = W+E+N+S, padx= 5)
 
 #***************************************************************************
 Units = Label(root, text = "Unit Selection").grid(row=0,column=0)
 imp = IntVar()
-imperial = Checkbutton(root, text = "imperial",anchor = SW,width = 20,bg ="khaki", variable = imp,onvalue = 1, offvalue = 0).grid(row = 1, column = 0)
+imperial = Checkbutton(root, text = "imperial",anchor = SW,width = 10,bg ="khaki", variable = imp,onvalue = 1, offvalue = 0).grid(row = 1, column = 0)
 
 met = IntVar()
-metric = Checkbutton(root,text ="metric",anchor= SW, width = 20, bg ="khaki",variable = met,onvalue = 1, offvalue = 0).grid(row=2,column=0)
+metric = Checkbutton(root,text ="metric",anchor= SW, width = 10, bg ="khaki",variable = met,onvalue = 1, offvalue = 0).grid(row=2,column=0)
 
 #****************************************************************************
 
 
 # ***************************************************************************
+Frame2 = Frame(root,bg="lightcyan1",borderwidth=5, relief="sunken", width=300, height=220)
+Frame2.grid(row = 0, column = 2, rowspan = 7, columnspan = 2, sticky = W+E+N+S)
 
+Port_Title = Label(root, text= "Port Selection Menu ", bg ="lightcyan1").grid(row =0, column =2)
 Avail = Listbox(root)
+Avail.config(width=30)
 Avail.insert(1,"GPIO")
 Avail.insert(2,"USB1")
 Avail.insert(3,"USB2")
 Avail.config(width = 25)
-Avail.grid(row=1,column=1)
+Avail.grid(row=1,column=2,rowspan =5, columnspan = 1)
 
 #****************************************************************************
-Frame2 = Frame(root, bg="navy")
-Frame2.grid(row = 7, column = 0, rowspan = 4, columnspan = 2, sticky = W+E+N+S)
+Frame2 = Frame(root, bg="navy",relief="sunken", width=420, height=30)
+Frame2.grid(row = 7, column = 0, rowspan = 1, columnspan = 3, sticky = W+E+N+S)
 Label(root, bg ="navy").grid(row=7, column=0)
 
-Update = Button(root, text =" update settings", width = 15, height=1).grid(row=8, column = 0)
+update = Button(root, text =" Update Settings ", width = 15, height=1)
+update.grid(row=7, column = 2)
+update.bind("<Enter>",hover_color())
+
+cancel= Button(root, text =" Cancel", width = 10, height=1)
+cancel.grid(row=7,column= 0)
 
 root.mainloop()
