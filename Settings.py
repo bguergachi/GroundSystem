fd7f47d9be26d655de9eefa9ebd40e788a0e8a5b
ZavSer <ma.zavalaserret@gmail.com>
2017-01-28 17:33:00 -0500

    Select either metric or imperial, status enables for both. Error. Need to fix

--- Settings.py
+++ Settings.py
@@ -17,15 +17,15 @@ def close_popup(root):
 def cancel_confirm():
     popup = Tk()
     popup.geometry('200x70')
-    popup.title('Abandon Settings')
+    popup.title('Confirm Updates')
 
-    cancel_txt= Label(popup, text="No unsaved changes \n will be applied. ")
-    cancel_txt.pack()
+    update_txt = Label(popup, text="Unsaved setting will be lost. ")
+    update_txt.pack()
 
-    cancel_bttn= Button(popup,text= "OK")
-    cancel.bind("<Button-1>", lambda event: close_popup(popup))
-    cancel_bttn.pack()
-    return()
+    update_bttn = Button(popup, text="OK")
+    update_bttn.bind("<Button-1>", lambda event: close_popup(popup))
+    update_bttn.pack()
+    return ()
 
 def update_confirm():
     popup = Tk()
@@ -72,36 +72,36 @@ root.geometry('422x250') # this function defines the size of the window
 root.title('Settings Menu') # this function names the overall window
 root.configure( background = "snow",)
 
-Frame1 = Frame(root, bg="khaki",relief="sunken", width=100, height=220)
-Frame1.grid(row = 0, column = 0, rowspan = 3, columnspan = 2, sticky = W+E+N+S, padx= 5)
+frame1 = Frame(root, bg="khaki",relief="sunken", width=100, height=220)
+frame1.grid(row = 0, column = 0, rowspan = 3, columnspan = 2, sticky = W+E+N+S, padx= 5)
 
 #************** UNITS SLECTION SUBMENU***********************************
-Units = Label(root, text = "Unit Selection", bg="khaki").grid(row=0,column=0)
+units = Label(root, text = "Unit Selection", bg="khaki").grid(row=0,column=0)
 imp = IntVar()
 met = IntVar()
 
-imperial = Checkbutton(root, text = "imperial",anchor = SW,width = 10,bg ="khaki", variable = imp,onvalue = 1, offvalue = 0, command = disable_check(imp,met))
-#imperial.bind("<Button-1>", lambda event: disable_check(imp,met))
+imperial = Checkbutton(root, text = "imperial",anchor = SW,width = 10,bg ="khaki", variable = imp)
+imperial.bind("<Button-1>", lambda event: disable_check(imp,met))
 imperial.grid(row = 1, column = 0)
 
-metric = Checkbutton(root,text ="metric",anchor= SW, width = 10, bg ="khaki",variable = met,onvalue = 1, offvalue = 0, command= disable_check(imp,met))
-#metric.bind("<Button-1>", lambda event: disable_check(imp,met))
+metric = Checkbutton(root,text ="metric",anchor= SW, width = 10, bg ="khaki",variable = met)
+metric.bind("<Button-1>", lambda event: disable_check(imp,met))
 metric.grid(row=2,column=0)
 #****************************************************************************
 
 
 # *******************AVAILABLE PORTS LIST ******************************
-Frame2 = Frame(root,bg="lightcyan1",borderwidth=5, relief="sunken", width=300, height=220)
-Frame2.grid(row = 0, column = 2, rowspan = 7, columnspan = 2, sticky = W+E+N+S)
+frame2 = Frame(root,bg="lightcyan1",borderwidth=5, relief="sunken", width=300, height=220)
+frame2.grid(row = 0, column = 2, rowspan = 7, columnspan = 2, sticky = W+E+N+S)
 
-Port_Title = Label(root, text= "Port Selection Menu ", bg ="lightcyan1").grid(row =0, column =2)
-Avail = Listbox(root)
-Avail.config(width=30)
-Avail.insert(1,"GPIO")
-Avail.insert(2,"USB1")
-Avail.insert(3,"USB2")
-Avail.config(width = 25)
-Avail.grid(row=1,column=2,rowspan =5, columnspan = 1)
+port_Title = Label(root, text= "Port Selection Menu ", bg ="lightcyan1").grid(row =0, column =2)
+avail = Listbox(root)
+avail.config(width=30)
+avail.insert(1,"GPIO")
+avail.insert(2,"USB1")
+avail.insert(3,"USB2")
+avail.config(width = 25)
+avail.grid(row=1,column=2,rowspan =5, columnspan = 1)
 
 #************UPDATE/CANCEL BUTTONS *************************************
 Frame2 = Frame(root, bg="navy",relief="sunken", width=420, height=30)
@@ -114,7 +114,7 @@ update.bind("<B1-Motion>", lambda event:hover_color(update,"green"))
 update.grid(row=7, column = 2)
 
 cancel= Button(root, text =" Cancel", width = 10, height=1)
-cancel.bind("<Button-1>",lambda event: cancel_confirm())
+cancel.bind("<Button-1>",lambda event: update_confirm())
 cancel.grid(row=7,column= 0)
 
 root.mainloop()
