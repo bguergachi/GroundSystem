4c69c794d5c6bd3fb32567c7c9e997a4e76abe5b
ZavSer <ma.zavalaserret@gmail.com>
2017-02-04 11:54:52 -0500

    unit selection, port selection function updated.

--- Settings.py
+++ Settings.py
@@ -3,35 +3,21 @@
 #@author: Alejandra Zavala
 
 from tkinter import *
-global imp, met, imperial,metric
-
+global units, ports
 #***************FUNCTIONS************************
 
 def hover_color(widget, color): # function changes color of button when hovering above it
-    widget.config(bg =color)
+    widget.config(bg =color) #backgrounf color widget configuration
 
-def close_popup(root):
-    root.destroy()
+def close_popup(root): # closes popup window , used in cancel_confirm function
+    root.destroy() # ___.destroy() closes the window
 
-def cancel_confirm():
+def update_confirm(mssg): #pop-up message window to confirm action
     popup = Tk()
     popup.geometry('200x70')
     popup.title('Confirm Updates')
 
-    update_txt = Label(popup, text="Unsaved setting will be lost. ")
-    update_txt.pack()
-
-    update_bttn = Button(popup, text="OK")
-    update_bttn.bind("<Button-1>", lambda event: close_popup(popup))
-    update_bttn.pack()
-    return ()
-
-def update_confirm():
-    popup = Tk()
-    popup.geometry('200x70')
-    popup.title('Confirm Updates')
-
-    update_txt=Label(popup, text="New settings \nhave been saved. ")
+    update_txt=Label(popup, text= mssg)
     update_txt.pack()
 
     update_bttn=Button(popup,text= "OK")
@@ -39,13 +25,21 @@ def update_confirm():
     update_bttn.pack()
     return()
 
-def get_availPorts():
-    return("port1,port2")
+def send_settings(): # function that tells status menu which units to display
+    if units==1: # radio button value for imperial units is 1
+        print("display imperial units")
+    else:
+        print("display metric units")
 
-def select_port():
-    return()
+# need to update once we figure out the uart stuff
+def get_availPorts(): #function outputs list of ports avilable
+    ports = ["port1", "port2", "port3"]
+    return(ports)
 
-
+# need to update once we figure out the uart stuff
+def select_port(listb): # sends port selection to pywire
+    list_num = listb.curselection() # outputs the index of the selected port from the listbox
+    return(list_num)
 
 # *****************************GUI PORTION************************************
 #*****************************************************************************
@@ -59,18 +53,15 @@ frame1 = Frame(root, bg="khaki",relief="sunken", width=100, height=220)#everythi
 frame1.grid(row = 0, column = 0, rowspan = 3, columnspan = 2, sticky = W+E+N+S, padx= 5) # formatting locations and span of widget
 
 #************** UNITS SLECTION SUBMENU***********************************
-unit_label = Label(root, text = "Unit Selection", bg="khaki").grid(row=0,column=0)
-units= IntVar()
+unit_label = Label(root, text = "Unit Selection", bg="khaki").grid(row=0,column=0)#title of subsection
+units= IntVar()# variable will hold variable value assigned to each radiobutton
 
+#used radiobutton because they only admit one selection (either metric or imperial)
 imperial = Radiobutton(root, text = "imperial",anchor = SW,width = 10,bg ="khaki", variable = units,value =1)
-#imperial.bind("<Button-1>", lambda event: disable_check(imperial,metric))
 imperial.grid(row = 1, column = 0)
 
 metric = Radiobutton(root,text ="metric",anchor= SW, width = 10, bg ="khaki",variable = units,value=2)
-#metric.bind("<Button-1>", lambda event: disable_check(imperial,metric))
 metric.grid(row=2,column=0)
-#****************************************************************************
-
 
 # *******************AVAILABLE PORTS LIST ******************************
 frame2 = Frame(root,bg="lightcyan1",borderwidth=5, relief="sunken", width=300, height=220)
@@ -78,12 +69,17 @@ frame2.grid(row = 0, column = 2, rowspan = 7, columnspan = 2, sticky = W+E+N+S)
 
 port_Title = Label(root, text= "Port Selection Menu ", bg ="lightcyan1").grid(row =0, column =2)
 avail = Listbox(root)
-avail.config(width=30)
-avail.insert(1,"GPIO")
-avail.insert(2,"USB1")
-avail.insert(3,"USB2")
-avail.config(width = 25)
+
+ports = get_availPorts() # function outputs a list with available ports
+
+for port in ports: # for all items in the ports list
+    avail.insert(END,port) #insert the item in the listbox
+
+avail.config(width = 25)#formatting
+avail.bind('<<ListboxSelect>>',lambda event:select_port(avail))
 avail.grid(row=1,column=2,rowspan =5, columnspan = 1)
+
+
 
 #************UPDATE/CANCEL BUTTONS *************************************
 Frame2 = Frame(root, bg="navy",relief="sunken", width=420, height=30)
@@ -91,12 +87,11 @@ Frame2.grid(row = 7, column = 0, rowspan = 1, columnspan = 3, sticky = W+E+N+S)
 Label(root, bg ="navy").grid(row=7, column=0)
 
 update = Button(root, text =" Update Settings ", width = 20, height=1)
-update.bind("<Button-1>",lambda event: update_confirm())
-update.bind("<B1-Motion>", lambda event:hover_color(update,"green"))
+update.bind("<Button-1>",lambda event: update_confirm("New settings \nhave been saved. "))
 update.grid(row=7, column = 2)
 
 cancel= Button(root, text =" Cancel", width = 10, height=1)
-cancel.bind("<Button-1>",lambda event: update_confirm())
+cancel.bind("<Button-1>",lambda event: update_confirm("Unsaved setting will be lost. "))
 cancel.grid(row=7,column= 0)
 
 root.mainloop()
