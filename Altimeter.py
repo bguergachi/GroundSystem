6f92fcc8f7700d451992298514ef63177f579079
Shivam Saheb <gryfphen@gmail.com>
2017-02-04 21:02:35 -0500

    Created loops within functions Created and tested new Time Variables to simulate live updates Recreated label to match updated loops Reorganized functions Reorganized loops within function Removed unneccessary binds

--- Altimeter.py
+++ Altimeter.py
@@ -1,68 +1,99 @@
 from tkinter import *
 import random
+import time
 
-def window (main):
-    main.title ('Altimeter')
-    height=250
-    width=422
+def window(main):
+    main.title('Altimeter')
+    height = 250
+    width = 422
     root.geometry('{}x{}'.format(width, height))
 
-def printaltitude(e):
-    sensor.config(text='blue')
+def printaltitude():
+    global state
+    state=0
 
-def printtemperature(e):
-    sensor.config(text='red')
+def printtemperature():
+    global state
+    state = 1
 
-def printpressure(e):
-    sensor.config(text='orange')
+def printpressure():
+    global state
+    state = 2
 
-def printairspeed(e):
-    sensor.config(text='green')
+def printairspeed():
+    global state
+    state = 3
+    '''sensor.config(text='green')'''
 
 root = Tk()
 root.resizable(width=False, height=False)
 
 window(root)
 
-#This is the frame where the buttons are located
+# This is the frame where the buttons are located
 mainFrame = Frame(root)
 mainFrame.pack()
 
-#This is the frame where the buttons are located
+# This is the frame where the buttons are located
 buttonFrame = Frame(mainFrame, height=250, width=1140)
 buttonFrame.pack(side=LEFT, anchor=W)
 
-#This is the frame where the values are located
-metricFrame= Frame(mainFrame, height=250, width=240, bg='cyan')
+# This is the frame where the value label is located
+metricFrame = Frame(mainFrame, height=250, width=240, bg='cyan')
 metricFrame.pack_propagate(False)
 metricFrame.pack(side=LEFT, anchor=W)
 
-sensor = Label(metricFrame, height=50,width=200, font='size, 20', text='shiaf')
+var1 = StringVar(metricFrame)
+
+# This is the value label which is used to display the live values
+sensor = Label(metricFrame, height=50, width=200, font='size, 25', fg='green', textvariable=var1)
 sensor.place(relx=0.5, rely=0.5, anchor=CENTER)
 sensor.pack()
 
 # This is the configuration for the Altitude Button. It includes the location, size of font, and the print command
-altitude = Button(buttonFrame, text='Altitude', bg='red', fg='white')
-altitude.bind("<Button-1>", printaltitude)
+altitude = Button(buttonFrame, text='Altitude', bg='red', fg='white', command=printaltitude)
+'altitude.bind("<Button-1>", printaltitude)'
 altitude.config(font=("times,12"))
 altitude.pack(side=BOTTOM, fill=BOTH, anchor=W)
 
 # This is the configuration for the Temperature Button. It includes the location, size of font, and the print command
-temperature = Button(buttonFrame, text='Temperature', bg='cyan', fg='black')
-temperature.bind("<Button-1>", printtemperature)
+temperature = Button(buttonFrame, text='Temperature', bg='cyan', fg='black', command=printtemperature)
+'temperature.bind("<Button-1>", printtemperature)'
 temperature.config(font=("times,12"))
 temperature.pack(side=BOTTOM, fill=BOTH, anchor=W)
 
 # This is the configuration for the Pressure Button. It includes the location, size of font, and the print command
-pressure = Button(buttonFrame, text='Pressure', bg='red', fg='white')
-pressure.bind("<Button-1>", printpressure)
+pressure = Button(buttonFrame, text='Pressure', bg='red', fg='white', command=printpressure)
+'pressure.bind("<Button-1>", printpressure)'
 pressure.config(font=("times,12"))
 pressure.pack(side=BOTTOM, fill=BOTH, anchor=W)
 
 # This is the configuration for the Airspeed Button. It includes the location, size of font, and the print command
-airspeed = Button(buttonFrame, text='Airspeed', bg='cyan', fg='black')
-airspeed.bind("<Button-1>", printairspeed)
+airspeed = Button(buttonFrame, text='Airspeed', bg='cyan', fg='black', command=printairspeed)
+'airspeed.bind("<Button-1>", printairspeed)'
 airspeed.config(font=("times,12"))
 airspeed.pack(side=BOTTOM, fill=BOTH, anchor=W)
+
+state = -1
+
+def update():
+    displayText = ''
+    if state == -1:
+        displayText = 'Initiate'
+    elif state == 0:
+        displayText = 'Altitude'
+    elif state == 1:
+        displayText = 'Temperature'
+    elif state == 2:
+        displayText = 'Pressure'
+    elif state == 3:
+        displayText = 'Airspeed'
+
+    var1.set(displayText)
+    metricFrame.update()
+
+def mainloop():
+    while True:
+        update()
 
 mainloop()
