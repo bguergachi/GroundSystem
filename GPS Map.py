be4cb0ced5911f9e91f6ab8f73b783c8bf1d988d
amuntean522 <amunteanu@ryerson.ca>
2017-02-16 22:00:40 -0500

    Added Class, Added Large Map, Started Path, Class needs fixing

--- GPS Map.py
+++ GPS Map.py
@@ -5,87 +5,125 @@ from time import sleep
 import os,sys,random
 from PIL import ImageTk,Image
 
-root = Tk()
-
 #Defines frame dimensions
 height=250
 width=422
 
-root.resizable(width=False, height=False)
-root.geometry('{}x{}'.format(width, height))
+#Loads Small Map
+load_smallmap = Image.open("Map Image.png")
+load_smallmap.thumbnail(size=(width, height))
 
-frame = Frame(root)
-frame.pack()
-
-#Loads Map
-load_map = Image.open("Map Image.png")
-load_map.thumbnail(size=(width, height))
+#Loads Large Map
+load_largemap = Image.open("Large Map.png")
+load_largemap.thumbnail(size=(width, height))
 
 #Loads Crosshair
 load_crosshair = Image.open("Crosshair.png")
 crosshair_size = (20, 20)
 load_crosshair.thumbnail(size=(crosshair_size[0], crosshair_size[1]))
 
-'''Coordinates of Map corners
-topleft = 32.955651, -106.930123
-topright = 32.955651, -106.892924
-botleft = 32.937436, -106.930123
-botright = 32.937436, -106.892924
+'''
+smallmap_corners = {topleft: [32.955651, -106.930123], topright: [32.955651, -106.892924],
+                    botleft: [32.937436, -106.930123], botright: [32.937436, -106.892924]}
+largemap_corners = {topleft: [32.979024, -106.967445], topright: [32.979024, -106.858726],
+                    botleft: [32.925260, -106.930123], botright: [32.925260, -106.858726]}
 '''
 
-#Sets length and width of map frame in terms of latitude and longitude coordinates
-latitude_top = 32.955651
-latitude_bot = 32.937436
-latitude_length = latitude_top - latitude_bot
+class Map:
 
-longitude_right = 106.892924
-longitude_left = 106.930123
-longitude_length = longitude_left - longitude_right
+    # Coordinates of Small and Large Map sides
+    smallmap_side = {'top': 32.955651, 'bot': 32.937436, 'left': -106.930123, 'right': -106.892924}
+    largemap_side = {'top': 32.979024, 'bot': 32.925260, 'left': -106.967445, 'right': -106.858726}
+    map_side = smallmap_side.copy()  # map being displayed
 
-#Derives proportionality between latitude/longitude coordinates and x,y direction pixels
-y_relation = height/latitude_length
-x_relation = width/longitude_length
+    # ***************** Instantiate *****************
+    def __init__(self, master):
+        self.__master = master
+        # Set window parameters
+        self.__master.resizable(width=False, height=False)
+        self.__master.geometry('{}x{}'.format(width, height))
+        self.frame = Frame(self.__master)
+        self.frame.pack()
 
-#Converts latitude/longitude coordinates to x,y parameters
-def convert_pixel(latitude, longitude):
-    y = (latitude_top-latitude)*y_relation
-    x = (longitude_left-(longitude*(-1)))*x_relation
-    return y, x
+        self.__random_coordinate = self.getRandomNumber()
+        self.frame_dimensions()
+        self.choose_map()
+        self.convert_pixel()
+        self.display_map()
 
-#Random test coordinates
-def getRandomNumber():
-    rand_lat = round(random.uniform(latitude_bot, latitude_top), 6)
-    rand_long = round(random.uniform(longitude_right, longitude_left), 6)*(-1)
-    return rand_lat, rand_long
+    # Sets length, width, and border values of map frame in terms of latitude and longitude coordinates
+    def frame_dimensions(self):
+        self.__latitude_top = self.map_side['top']
+        self.__latitude_bot = self.map_side['bot']
+        self.__latitude_length = self.__latitude_top - self.__latitude_bot
 
-random_coordinate = getRandomNumber()
-print(random_coordinate)
+        self.__longitude_right = self.map_side['right']
+        self.__longitude_left = self.map_side['left']
+        self.__longitude_length = self.__longitude_left - self.__longitude_right
+        return [self.__latitude_length, self.__longitude_length]
 
-pixel_values = convert_pixel(random_coordinate[0], random_coordinate[1])
-print(pixel_values)
+    # Random test coordinates
+    def getRandomNumber(self):
+        self.__rand_lat = round(random.uniform(self.__latitude_bot, self.__latitude_top), 6)
+        self.__rand_long = round(random.uniform(self.__longitude_right, self.__longitude_left), 6) * (-1)
+        print(self.__rand_lat, self.__rand_long)
+        return [self.__rand_lat, self.__rand_long]
 
-#Seperates integer and decimals
-pixel_integer = [int(pixel_values[0]), int(pixel_values[1])]
-pixel_decimals = (pixel_values[0] % 1, pixel_values[1] % 1)
-print(pixel_integer)
-print(pixel_decimals)
+    # Derives proportionality between latitude/longitude coordinates and x,y direction pixels
+    # Converts latitude/longitude coordinates to x,y pixel parameters
+    def convert_pixel(self):
+        y_relation = height / self.__latitude_length
+        x_relation = width / self.__longitude_length
+        y = (self.__latitude_top - self.__rand_lat) * y_relation
+        x = (self.__longitude_left - (self.__rand_long * (-1))) * x_relation
+        # Seperates integers and decimals
+        self.__pixel_integer = [int(y), int(x)]
+        self.__pixel_decimals = (y % 1, x % 1)
 
-#Displays Map and Crosshair
-load_map.paste(load_crosshair,(pixel_integer[1]-int(crosshair_size[0]/2), pixel_integer[0]-int(crosshair_size[1]/2)), load_crosshair) #Positions and blends Crosshair with map
-map = ImageTk.PhotoImage(load_map)
-crosshair = ImageTk.PhotoImage(load_crosshair)
-label_map = Label(frame, image=map)
-label_map.pack()
+    # Displays Map and Crosshair
+    def display_map(self):
+        self.__load_map.paste(load_crosshair, (self.__pixel_integer[1] - int(crosshair_size[0] / 2), self.__pixel_integer[0] - int(crosshair_size[1] / 2)), load_crosshair)  # Positions and blends Crosshair with map
+        map = ImageTk.PhotoImage(self.__load_map)
+        crosshair = ImageTk.PhotoImage(load_crosshair)
+        label_map = Label(self.frame, image=map)
+        label_map.pack()
 
-#Notifies user if crosshair leaves map
-if random_coordinate[0] > latitude_top or random_coordinate[0] < latitude_bot:
-    tkinter.messagebox.showwarning('Warning', 'Rocket has left the competition area!!!')
-    print("Rocket has left competition area!!!")
-elif random_coordinate[1]*(-1) > longitude_left or random_coordinate[1]*(-1) < longitude_right:
-    tkinter.messagebox.showwarning('Warning', 'Rocket has left the competition area!!!')
-    print("Rocket has left competition area!!!")
+    # Chooses Map and notifies user if Rocket leaves Small Map
+    def choose_map(self):
+        if self.__random_coordinate[0] > self.__latitude_top or self.__random_coordinate[0] < self.__latitude_bot:
+            tkinter.messagebox.showwarning('Warning', 'Rocket has left the competition area!!!')
+            print("Rocket has left competition area!!!")
+            self.map_side = self.largemap_side.copy()
+            self.__load_map = load_largemap
+        elif self.__random_coordinate[1] * (-1) > self.__longitude_left or self.__random_coordinate[1] * (-1) < self.__longitude_right:
+            tkinter.messagebox.showwarning('Warning', 'Rocket has left the competition area!!!')
+            print("Rocket has left competition area!!!")
+            self.map_side = self.largemap_side.copy()
+            self.__load_map = load_largemap
+        elif self.__random_coordinate[0] < self.__latitude_top or self.__random_coordinate[0] > self.__latitude_bot:
+            self.map_side = self.smallmap_side.copy()
+            self.__load_map = load_smallmap
+        elif self.__random_coordinate[1] * (-1) < self.__longitude_left or self.__random_coordinate[1] * (-1) > self.__longitude_right:
+            self.map_side = self.smallmap_side.copy()
+            self.__load_map = load_smallmap
 
 
+
+#Displays path of Crosshair
+'''
+path_array = [[32.940321, -106.913103], [32.941321, -106.913103], [32.942321, -106.913103], [32.943321, -106.913103], [32.945321, -106.913103]]
+
+def getCoordinates():
+    for i in range(5):
+        path_coordinate = path_array[i]
+    root.after(500, getCoordinates)
+    return path_coordinate
+
+root.after(0, getCoordinates)
+'''
+
+root = Tk()
+run_map = Map(root)
 root.mainloop()
 
 
