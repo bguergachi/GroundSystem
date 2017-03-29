42f006eefd71cbe7101d3cce0c9cb2dbdb84b8fb
aguergachi <bguergachi@gmail.com>
2017-03-29 18:53:44 -0400

    Serial Testing now works
    Testing for uart works

--- SerialPart/sirialSig/newmain.c
+++ SerialPart/sirialSig/newmain.c
@@ -18,26 +18,48 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
-int main() {
-    int fd;
-    int count;
-    unsigned int nextTime;
+int main ()
+{
+  int fd ;
+  int count ;
+  unsigned int nextTime ;
 
-    if ((fd = serialOpen("/dev/ttyAMA0", 115200)) < 0) {
-        fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
-        return 1;
+  if ((fd = serialOpen ("/dev/ttyS0", 115200)) < 0)
+  {
+    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+  if (wiringPiSetupGpio () == -1)
+  {
+    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+  nextTime = millis () + 300 ;
+
+  for (count = 0 ; count < 256 ; )
+  {
+    if (millis () > nextTime)
+    {
+      printf ("\nOut: %3d: ", count) ;
+      fflush (stdout) ;
+      serialPutchar (fd, count) ;
+      nextTime += 300 ;
+      ++count ;
     }
 
-    if (wiringPiSetup() == -1) {
-        fprintf(stdout, "Unable to start wiringPi: %s\n", strerror(errno));
-        return 1;
+    delay (1000) ;
+
+    while (serialDataAvail (fd))
+    {
+      
+      printf (" -> %3d", serialGetchar (fd)) ;
+      fflush (stdout) ;
     }
+  }
 
-    serialPutchar(fd,5);
-    
-    printf("%d",serialGetchar(fd));
-
-    printf("\n");
-    return 0;
+  printf ("\n") ;
+  return 0 ;
 }
 
