daf986e03199c3ff6c0307ab4c97d1419ff53124
aguergachi <bguergachi@gmail.com>
2018-03-15 17:16:26 -0400

    More arduino files
    Added files for testing serial comunication

--- Arduino/serial/serial.ino
+++ Arduino/serial/serial.ino
@@ -2,19 +2,19 @@ char a;
 
 
 void setup() {
-  Serial.begin(9600);              //Starting serial communication
+  Serial.begin(57600);              //Starting serial communication
   pinMode(LED_BUILTIN, OUTPUT);
 }
   
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on
-  Serial.write("\n50238\r");
+  Serial.write("\nGood\r");
   Serial.write("\n5890231\r");
   Serial.write("\n453623\r");
-  Serial.write("\n4536722\r");
-  Serial.write("\n45734573467\r");
+  Serial.write("\nHello\r");
+  Serial.write("\n95734573467\r");
   Serial.write("\n3489560123.4112334\r");
-  delay(5000);
+  delay(500);
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED on
-  delay(5000);
+  delay(500);
 }
