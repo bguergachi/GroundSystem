82561a0e95018ed89c067128e40063e15b8ad849
Shivam Saheb <gryfphen@gmail.com>
2018-05-03 14:51:58 -0400

    System Plots
    Commit #: 4
    
    Cleaned up code with classes
    Fixed some minor errors
    Made plot read and update from txt file "sampleData.txt"

--- src/System_Plots.py
+++ src/System_Plots.py
@@ -13,9 +13,6 @@ import random
 height = 422
 width = 250
 
-# f = Figure(figsize=(5,5), dpi=100)
-# a = f.add_subplot(111)
-
 class Display:
     def __init__(self, master):
         self.__master = master
@@ -47,101 +44,116 @@ class Display:
         fullFrame.pack(side=BOTTOM)
 
         self.avt = Button(fullFrame, text = "Alt - Time", height= 2, width=12, bg='Cyan')
-        self.avt.bind("<Button-1>", self.__graph1)
+        self.avt.bind("<Button-1>", self.animate)
         self.avt.pack(side=LEFT, pady=6, padx=4)
+
         self.irvt = Button(fullFrame, text = "IR - Time", height= 2, width=12, bg='Cyan')
-        self.irvt.bind("<Button-1>", self.__graph2)
+        self.irvt.bind("<Button-1>", self.animate)
         self.irvt.pack(side=LEFT, pady=6, padx=4)
+
         self.pvt = Button(fullFrame, text = "Press - Time", height= 2, width=12, bg='Cyan')
-        self.pvt.bind("<Button-1>", self.__graph3)
+        self.pvt.bind("<Button-1>", self.animate)
         self.pvt.pack(side=LEFT, pady=6, padx=4)
+
         self.tvt = Button(fullFrame, text = "Temp - Time", height= 2, width=12, bg='Cyan')
-        self.tvt.bind("<Button-1>", self.__graph4)
+        self.tvt.bind("<Button-1>", self.animate)
         self.tvt.pack(side=LEFT, pady=6, padx=4)
 
-    # def animate(self, i, event):
+    def animate(self, event):
+
+        if self.widget:
+            self.widget.destroy()
+
+        pullData = open("sampleData.txt", "r").read()
+        dataList = pullData.split('\n')
+        xList = []
+        yList = []
+        for eachLine in dataList:
+            if len(eachLine) > 1:
+                x, y = eachLine.split(',')
+                xList.append(int(x))
+                yList.append(int(y))
+
+        f = Figure(figsize=(5, 5), dpi=100)
+        a = f.add_subplot(111)
+        f.subplots_adjust(bottom=0.15)
+
+        a.clear()
+        a.plot(xList, yList)
+
+        canvas = FigureCanvasTkAgg(f, self.__master)
+        canvas.draw()
+        self.widget = canvas.get_tk_widget()
+        self.widget.pack()
+
+        ani = animation.FuncAnimation(f, self.animate, interval=1000)
+
+
+
+
+    # def __graph1(self, event):
     #
     #     if self.widget:
     #         self.widget.destroy()
     #
-    #     pullData = open("sampleData.txt", "r").read()
-    #     dataList = pullData.split('\n')
-    #     xList = []
-    #     yList = []
-    #     for eachLine in dataList:
-    #         if len(eachLine) > 1:
-    #             x, y = eachLine.split(',')
-    #             xList.append(int(x))
-    #             yList.append(int(y))
+    #     f = Figure()
+    #     a = f.add_subplot(111)
+    #     f.subplots_adjust(bottom=0.15)
+    #     a.plot([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],[0,30,90,150,250,400,750,1250,1750,2500,1750,1250,750,400,250,150,90,30,15,10,0])
     #
-    #     a.clear()
-    #     a.plot(xList, yList)
-
-
-    def __graph1(self, event):
-
-        if self.widget:
-            self.widget.destroy()
-
-        f = Figure()
-        a = f.add_subplot(111)
-        f.subplots_adjust(bottom=0.15)
-        a.plot([1,2,3],[4,5,4])
-
-        canvas = FigureCanvasTkAgg(f, self.__master)
-        canvas.draw()
-        self.widget = canvas.get_tk_widget()
-        self.widget.pack()
-
-    def __graph2(self, event):
-
-        if self.widget:
-            self.widget.destroy()
-
-        f = Figure()
-        a = f.add_subplot(111)
-        f.subplots_adjust(bottom=0.15)
-        a.plot([1,2,3],[2,2.5,2])
-
-        canvas = FigureCanvasTkAgg(f, self.__master)
-        canvas.draw()
-        self.widget = canvas.get_tk_widget()
-        self.widget.pack()
-
-    def __graph3(self, event):
-
-        if self.widget:
-            self.widget.destroy()
-
-        f = Figure()
-        a = f.add_subplot(111)
-        f.subplots_adjust(bottom=0.15)
-        a.plot([1,2,3],[1,2,3])
-
-        canvas = FigureCanvasTkAgg(f, self.__master)
-        canvas.draw()
-        self.widget = canvas.get_tk_widget()
-        self.widget.pack()
-
-    def __graph4(self, event):
-
-        if self.widget:
-            self.widget.destroy()
-
-        f = Figure()
-        a = f.add_subplot(111)
-        f.subplots_adjust(bottom=0.15)
-        a.plot([1,2,3],[-1,-4,-6])
-
-        canvas = FigureCanvasTkAgg(f, self.__master)
-        canvas.draw()
-        self.widget = canvas.get_tk_widget()
-        self.widget.pack()
+    #     canvas = FigureCanvasTkAgg(f, self.__master)
+    #     canvas.draw()
+    #     self.widget = canvas.get_tk_widget()
+    #     self.widget.pack()
+    #
+    # def __graph2(self, event):
+    #
+    #     if self.widget:
+    #         self.widget.destroy()
+    #
+    #     f = Figure()
+    #     a = f.add_subplot(111)
+    #     f.subplots_adjust(bottom=0.15)
+    #     a.plot([1,2,3],[2,2.5,2])
+    #
+    #     canvas = FigureCanvasTkAgg(f, self.__master)
+    #     canvas.draw()
+    #     self.widget = canvas.get_tk_widget()
+    #     self.widget.pack()
+    #
+    # def __graph3(self, event):
+    #
+    #     if self.widget:
+    #         self.widget.destroy()
+    #
+    #     f = Figure()
+    #     a = f.add_subplot(111)
+    #     f.subplots_adjust(bottom=0.15)
+    #     a.plot([1,2,3],[1,2,3])
+    #
+    #     canvas = FigureCanvasTkAgg(f, self.__master)
+    #     canvas.draw()
+    #     self.widget = canvas.get_tk_widget()
+    #     self.widget.pack()
+    #
+    # def __graph4(self, event):
+    #
+    #     if self.widget:
+    #         self.widget.destroy()
+    #
+    #     f = Figure()
+    #     a = f.add_subplot(111)
+    #     f.subplots_adjust(bottom=0.15)
+    #     a.plot([1,2,3],[-1,-4,-6])
+    #
+    #     canvas = FigureCanvasTkAgg(f, self.__master)
+    #     canvas.draw()
+    #     self.widget = canvas.get_tk_widget()
+    #     self.widget.pack()
 
 if __name__ == '__main__':
     root = Tk()
     root.title("System Plots")
-    # self.ani = animation.FuncAnimation(f, self.animate, interval=1000)
     display = Display(root)
     root.mainloop()
 
