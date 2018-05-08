c0a17db573df902caf5fd12cfd9b08fb18d064b4
Shivam Saheb <gryfphen@gmail.com>
2018-05-07 21:11:28 -0400

    System Plots
    Commit #: 8
    
    Cleaned up code
    
    To do:
     - Change Directories as required

--- src/System_Plots.py
+++ src/System_Plots.py
@@ -31,29 +31,29 @@ class Plot:
         fullFrame = Frame(self.__master)
         fullFrame.pack(side=BOTTOM)
 
-        self.__avt = Button(fullFrame, text = "Alt - Time", bg='darkcyan', fg = 'white', width = 10)
+        self.__avt = Button(fullFrame, text = "h - t", bg='darkcyan', fg = 'white', width = 7)
         self.__avt.bind("<Button-1>", lambda ev: self.__setPath("/../DataFiles/altitude.csv"))
-        self.__avt.grid(row = 0, column = 1, padx = 3, pady = 1)
+        self.__avt.grid(row = 0, column = 1, padx = 5)
 
-        self.__etvt = Button(fullFrame, text = "Ex. Temp - Time", bg='darkcyan', fg = 'white', width = 12)
+        self.__etvt = Button(fullFrame, text = "T - t", bg='darkcyan', fg = 'white', width = 7)
         self.__etvt.bind("<Button-1>", lambda ev: self.__setPath("/../DataFiles/temperature.csv"))
-        self.__etvt.grid(row = 0, column = 2, padx = 3, pady = 1)
+        self.__etvt.grid(row = 0, column = 2, padx = 5)
 
-        self.__irvt = Button(fullFrame, text = "IR - Time", bg='darkcyan', fg = 'white', width = 10)
+        self.__irvt = Button(fullFrame, text = "IR - t", bg='darkcyan', fg = 'white', width = 7)
         self.__irvt.bind("<Button-1>", lambda ev: self.__setPath("/../DataFiles/IRdistance.csv"))
-        self.__irvt.grid(row = 0, column = 3, padx = 3, pady = 1)
+        self.__irvt.grid(row = 0, column = 3, padx = 5)
 
-        self.__eacvt = Button(fullFrame, text = "Ex. Accel - Time", bg='darkcyan', fg = 'white', width = 15)
+        self.__eacvt = Button(fullFrame, text = "Ex. a - t", bg='darkcyan', fg = 'white', width = 7)
         self.__eacvt.bind("<Button-1>", lambda ev: self.__setPath("/../DataFiles/acceleration.csv"))
-        self.__eacvt.grid(row = 1, column = 1, padx = 3, pady = 3)
+        self.__eacvt.grid(row = 0, column = 4, padx = 5)
 
-        self.__iavt = Button(fullFrame, text = "In. Accel - Time", bg='darkcyan', fg = 'white', width = 15)
-        self.__iavt.bind("<Button-1>", lambda ev: self.__setPath("SECOND MPU"))
-        self.__iavt.grid(row = 1, column = 3, padx = 3)
+        self.__iavt = Button(fullFrame, text = "In. a - t", bg='darkcyan', fg = 'white', width = 7)
+        self.__iavt.bind("<Button-1>", lambda ev: self.__setPath("/../DataFiles/acceleration.csv"))
+        self.__iavt.grid(row = 0, column = 5, padx = 5)
 
-        self.__btvt = Button(fullFrame, text = "Bat. Temp - Time", bg='darkcyan', fg = 'white', width = 15)
-        self.__btvt.bind("<Button-1>", lambda ev: self.__setPath("BAT TEMP"))
-        self.__btvt.grid(row = 1, column = 2, padx = 3, pady = 3)
+        self.__btvt = Button(fullFrame, text = "B.T - t", bg='darkcyan', fg = 'white', width = 7)
+        self.__btvt.bind("<Button-1>", lambda ev: self.__setPath("/../DataFiles/acceleration.csv"))
+        self.__btvt.grid(row = 0, column = 6, padx = 5)
 
     def __setPath(self,filepath):
         self.__filepath = filepath
